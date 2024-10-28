5871cfc67c6596a8021c716c7da9fc434f2bd238
Faramires <amirfar@yandex.ru>
2024-10-29 01:13:10 +0300

    BEAUTIFIED CODE, PALINDROME_CHARSET IS CONST

--- js/functions.js
+++ js/functions.js
@@ -5,24 +5,18 @@ const CheckLength = (charset, length) => {
   const result = charset.length <= length;
   return result;
 };
-
 CheckLength (STRING_CHARSET, STRING_LENGTH);
 
-
-
 //ЗАДАНИЕ #2
-let PALINDROME_CHARSET = 'А Роза Упала На Лапу Азора';
+const PALINDROME_CHARSET = 'А Роза Упала На Лапу Азора';
 const CheckPalindrome = (charset) => {
   let reverse = '';
   charset = charset.toLowerCase();
   charset = charset.replace(/ /g, '');
-  for (let i = charset.length - 1; i >= 0 ; i--) {
+  for (let i = charset.length - 1; i >= 0; i--) {
     reverse += charset[i];
   }
   const result = charset === reverse;
   return result;
 };
-
 CheckPalindrome (PALINDROME_CHARSET);
-
-
