04c119df2073536aaa9bf8552f7e92c2526c7dfa
Faramires <amirfar@yandex.ru>
2024-11-12 11:50:32 +0300

    MODULE 5 TASK 1-2 INIT

--- js/functions.js
+++ js/functions.js
@@ -2,21 +2,45 @@
 const STRING_CHARSET = 'Любая строка';
 const STRING_LENGTH = 1;
 const CheckLength = (charset, length) => {
-  const result = charset.length <= length;
-  return result;
+    const result = charset.length <= length;
+    return result;
 };
-CheckLength (STRING_CHARSET, STRING_LENGTH);
+CheckLength(STRING_CHARSET, STRING_LENGTH);
 
 //ЗАДАНИЕ #2
 const PALINDROME_CHARSET = 'А Роза Упала На Лапу Азора';
 const CheckPalindrome = (charset) => {
-  let reverse = '';
-  charset = charset.toLowerCase();
-  charset = charset.replace(/ /g, '');
-  for (let i = charset.length - 1; i >= 0; i--) {
-    reverse += charset[i];
-  }
-  const result = charset === reverse;
-  return result;
+    let reverse = '';
+    charset = charset.toLowerCase();
+    charset = charset.replace(/ /g, '');
+    for (let i = charset.length - 1; i >= 0; i--) {
+        reverse += charset[i];
+    }
+    const result = charset === reverse;
+    return result;
 };
-CheckPalindrome (PALINDROME_CHARSET);
+CheckPalindrome(PALINDROME_CHARSET);
+
+//ЗАДАНИЕ 3
+/*
+'8:00' - начало рабочего дня
+'17:30' - конец рабочего дня
+'14:00' - начало встречи
+90 - продолжительность встречи в минутах
+*/
+
+const isOverTime = (startTime, endTime, meetingTime, meetingDuration) => {
+    const startDay = parseInt(startTime.split(':')[0]) * 60 + parseInt(startTime.split(':')[1]);
+    const endDay = parseInt(endTime.split(':')[0]) * 60 + parseInt(endTime.split(':')[1]);
+    const meetingStart = parseInt(meetingTime.split(':')[0]) * 60 + parseInt(meetingTime.split(':')[1]);
+    const meetingEnd = meetingStart + meetingDuration;
+    return meetingStart >= startDay && meetingEnd <= endDay;
+};
+
+isOverTime('8:00', '17:30', '08:00', 571); //false
+isOverTime('08:00', '17:30', '14:00', 90); // true
+isOverTime('8:0', '10:0', '8:0', 120);     // true
+isOverTime('08:00', '14:30', '14:00', 90); // false
+isOverTime('14:00', '17:30', '08:0', 90);  // false
+isOverTime('8:00', '17:30', '08:00', 900); // false
+
