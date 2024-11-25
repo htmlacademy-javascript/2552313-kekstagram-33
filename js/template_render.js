7d17f42994093b3f1b16c648dcb4e879a30b472a
Faramires <amirfar@yandex.ru>
2024-11-25 19:35:25 +0300

    FiXED SPACES

--- js/template_render.js
+++ js/template_render.js
@@ -5,17 +5,12 @@ const photoTemplate = document.querySelector('#picture').content.querySelector('
 const photoFragment = document.createDocumentFragment();
 
 photos.forEach(({url, description, likes, comments}) => {
-    const newThumbnail = photoTemplate.cloneNode(true);
-    newThumbnail.querySelector('.picture__img').src = url;
-    newThumbnail.querySelector('.picture__img').alt = description;
-    newThumbnail.querySelector('.picture__likes').textContent = likes;
-    newThumbnail.querySelector('.picture__comments').textContent = comments.length;
+  const newThumbnail = photoTemplate.cloneNode(true);
+  newThumbnail.querySelector('.picture__img').src = url;
+  newThumbnail.querySelector('.picture__img').alt = description;
+  newThumbnail.querySelector('.picture__likes').textContent = likes;
+  newThumbnail.querySelector('.picture__comments').textContent = comments.length;
   photoFragment.appendChild(newThumbnail);
   }
 );
 photoList.appendChild(photoFragment);
-
-
-
-
-
